6f7b6518ced51af591cbc1d90ff976641246e718
= <ashillpathak@gmail.com>
2020-03-23 17:36:45 +0000

    code

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package com.qa.main;
+
+public class HelloWorld {
+	
+	public void Print() {
+		System.out.println("Hello World!");
+	}
+	
+	public  void stringPrint(String word) {
+		
+		System.out.println(word);
+	}
+	
+	public static String stringReturn() {
+		
+		return "Hello World!";
+	}
+
+}
